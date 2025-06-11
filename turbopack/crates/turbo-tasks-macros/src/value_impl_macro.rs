acae3c680d4217ae507ff67492d12c31f08150ed
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-06-11 12:19:33 -0700

    FunctionMeta.immutable

--- turbopack/crates/turbo-tasks-macros/src/value_impl_macro.rs
+++ turbopack/crates/turbo-tasks-macros/src/value_impl_macro.rs
@@ -108,6 +108,7 @@ pub fn value_impl(args: TokenStream, input: TokenStream) -> TokenStream {
                     filter_trait_call_args: None, // not a trait method
                     local,
                     invalidator,
+                    immutable: sig.asyncness.is_none() && !invalidator,
                 };
 
                 let native_function_ident = get_inherent_impl_function_ident(ty_ident, ident);
@@ -227,6 +228,7 @@ pub fn value_impl(args: TokenStream, input: TokenStream) -> TokenStream {
                     filter_trait_call_args: turbo_fn.filter_trait_call_args(),
                     local,
                     invalidator,
+                    immutable: sig.asyncness.is_none() && !invalidator,
                 };
 
                 let native_function_ident =
